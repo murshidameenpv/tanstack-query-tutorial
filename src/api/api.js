8bc418ad6b76616ec2c6a396788519e41bd00b44
murshidameen <murshidmonzpv@gmail.com>
2024-02-22 21:05:17 +0530

    add post using usemutation

--- src/api/api.js
+++ src/api/api.js
@@ -5,3 +5,15 @@ export const fetchPosts = async () => {
   const data = response.data;
   return data;
 };
+export const fetchTags = async () => {
+  const response = await axios.get("http://localhost:3000/tags");
+  const data = response.data;
+  return data;
+};
+
+
+export const addPost = async (newPost) => {
+  const response = await axios.post("http://localhost:3000/posts", newPost);
+  const data = response.data;
+  return data;
+};
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,7 +1,5 @@
-
-
-import { useQuery } from "@tanstack/react-query";
-import { fetchPosts } from "../api/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { addPost, fetchPosts, fetchTags } from "../api/api";
 
 const PostList = () => {
   const {
@@ -13,18 +11,67 @@ const PostList = () => {
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
+  const { data: tagsData } = useQuery({
+    queryKey: ["tags"],
+    queryFn: fetchTags,
+  });
 
-  if (isLoading) return <p>Loading.....</p>;
-  if (isError) return <p>{error?.message}</p>;
+  const {
+    mutate,
+    isError: isPostError,
+    isPending,
+    error: postError,
+  } = useMutation({
+    mutationFn: addPost,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const postTitle = formData.get("title");
+    //it will create an array from iterable object.We want store all tags in single array
+    const postTags = Array.from(formData.keys()).filter(
+      (key) => formData.get(key) === "on"
+    );
+    if (!postTitle || !formData) return;
+    console.log(postTitle);
+    console.log(postTags);
+    mutate({ id:123, title: postTitle, tags: postTags });
+    e.target.reset();
+  };
 
   return (
-    <div className="container">
-      {postData && postData.map((post) => (
-        <div className="card" key={post.id}>
-          <h2>{post.title}</h2>
-          <p>Views: {post.views}</p>
+    <div>
+      <form className="post-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter Your Post" name="title" />
+        <div className="tags">
+          {tagsData?.map((tag) => (
+            <div key={tag} className="checkbox-container">
+              <input type="checkbox" name={tag} id={tag} />
+              <label htmlFor={tag}>{tag}</label>
+            </div>
+          ))}
         </div>
-      ))}
+        <button type="submit">Submit</button>
+      </form>
+      <div className="container">
+        {isLoading ? (
+          <p>Loading.....</p>
+        ) : isError ? (
+          <p>{error?.message}</p>
+        ) : (
+          postData?.map((post) => (
+            <div className="card" key={post.id}>
+              <h2>{post.title}</h2>
+              {post?.tags?.map((tag, index) => (
+                <p className="tag" key={index}>
+                  {tag}
+                </p>
+              ))}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
